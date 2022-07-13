03d8fa2d62f058f7cfb6afb2bfc4c82b3ebccc8d
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-13 16:51:10 +0530

    Update song.py

--- AsukaRobot/modules/song.py
+++ AsukaRobot/modules/song.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n**Duration**: `{duration}`\n **Views**: `{views}`\n **By**: [Akatsuki](t.me/Subhradeep00)'
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏳️**Duration**: `{duration}`\n🎑**Views**: `{views}`\n **By**: [Akatsuki](t.me/Subhradeep00)'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
