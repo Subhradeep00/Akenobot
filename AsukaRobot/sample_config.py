ce9d3f6386e940c7de13bd6a0a2aa6d080fb3d7d
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-14 15:39:50 +0530

    Update sample_config.py

--- AsukaRobot/sample_config.py
+++ AsukaRobot/sample_config.py
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = "awoo"  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "awoo"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5132611794 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 5546958418# If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "awoo"
-    SUPPORT_CHAT = 'AsukaSupport'  #Your own group for support, do not add the @
+    SUPPORT_CHAT = 'AkenoSupport00'  #Your own group for support, do not add the @
     JOIN_LOGGER = -123456789  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -123446789  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
