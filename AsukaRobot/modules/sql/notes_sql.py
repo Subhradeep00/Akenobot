12414211a5367b5890f508c4e9848378f51f8eee
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-17 20:22:05 +0530

    Update notes_sql.py

--- AsukaRobot/modules/sql/notes_sql.py
+++ AsukaRobot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from AsukaRobot.modules.helper_funcs.msg_types import Types
-from AsukaRobot.modules.sql import BASE, SESSION
+from Akenobot.modules.helper_funcs.msg_types import Types
+from Akenobot.modules.sql import BASE, SESSION
 from sqlalchemy import (Boolean, Column, Integer, String, UnicodeText, distinct,
                         func)
 
