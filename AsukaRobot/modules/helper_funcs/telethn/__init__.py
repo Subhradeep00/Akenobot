6eb72fb15f9b288954130e8be40a4ffe82267469
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-17 19:45:28 +0530

    Update __init__.py

--- AsukaRobot/modules/helper_funcs/telethn/__init__.py
+++ AsukaRobot/modules/helper_funcs/telethn/__init__.py
@@ -1,4 +1,4 @@
-from AsukaRobot import (DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn)
+from Akenobot import (DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES, telethn)
 
 IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
 
