65ee76f450cd2434ac207bf09539173c9c8b1aea
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-17 20:08:06 +0530

    Update antiflood_sql.py

--- AsukaRobot/modules/sql/antiflood_sql.py
+++ AsukaRobot/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, BigInteger, UnicodeText
 
-from AsukaRobot.modules.sql import SESSION, BASE
+from Akenobot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
