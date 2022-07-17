bebff9dce1bc32939d5081b6239ae6d6f0256efa
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-17 19:59:08 +0530

    Update extraction.py

--- AsukaRobot/modules/helper_funcs/extraction.py
+++ AsukaRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from AsukaRobot import LOGGER
-from AsukaRobot.modules.users import get_user_id
+from Akenobot import LOGGER
+from Akenobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
