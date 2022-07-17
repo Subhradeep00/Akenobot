3a8561aa457e7976624ba8921cd1817ef08c79cc
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-17 20:03:56 +0530

    Update msg_types.py

--- AsukaRobot/modules/helper_funcs/msg_types.py
+++ AsukaRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from AsukaRobot.modules.helper_funcs.string_handling import \
+from Akenobot.modules.helper_funcs.string_handling import \
     button_markdown_parser
 from telegram import Message
 
