98372e982424ad14bc53c0f8d6f10d5ca9ed3648
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-17 19:37:51 +0530

    Update errors.py

--- AsukaRobot/utils/errors.py
+++ AsukaRobot/utils/errors.py
@@ -2,7 +2,7 @@
 import sys
 import traceback
 from functools import wraps
-from AsukaRobot import pgram, SUPPORT_CHAT
+from Akenobot import pgram, SUPPORT_CHAT
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
 
